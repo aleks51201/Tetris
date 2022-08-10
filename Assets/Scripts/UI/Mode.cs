51682bc33a921d2138b7c529ca8c69d7653e7f4c
KipoGo <vit-tan@mail.ru>
2022-08-10 21:39:36 +0300

    Add new feature

---
+++ Assets/Scripts/UI/Mode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+class Mode:MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private GameObject preview;
+
+    private Animator previewAnimator;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        previewAnimator.SetTrigger("Start");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        previewAnimator.SetTrigger("Stop");
+    }
+    private void Start()
+    {
+        previewAnimator = preview.GetComponent<Animator>();
+    }
+}
