82357f001e249360da554d5bd228acffbb7ba3e9
KipoGo <vit-tan@mail.ru>
2022-09-18 23:41:03 +0300

    Fix bug
    Fix in game button animation

---
+++ Assets/Scripts/UI/CloseAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace Assets.Scripts.UI
+{
+    class CloseAnimation: MonoBehaviour
+    {
+        public void SetActive()
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
